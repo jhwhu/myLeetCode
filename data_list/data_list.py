8675088ed209a5b82d2a619b4a0558d36b71bbf9
jianghao1@pcitech.com <jianghao1@pcitech.com>
2020-06-04 08:56:06 +0800

    commit leetcode related

--- data_list/data_list.py
+++ data_list/data_list.py
@@ -57,20 +57,68 @@ class Solution28:
 #
 # 输入：arr = [17,18,5,4,6,1]
 # 输出：[18,6,6,6,1,-1]
+# #
+# class Solution1299:
+#     def replaceElements(self, arr):
+#         arr.append(-1)
+#         cur_max = -1
+#         for i in range(len(arr)-1, -1, -1):
+#             tmp = arr[i]
+#             arr[i] = cur_max
+#             cur_max = max(cur_max, tmp)
+#         return arr[:-1]
 #
-class Solution1299:
-    def replaceElements(self, arr):
-        arr.append(-1)
-        cur_max = -1
-        for i in range(len(arr)-1, -1, -1):
-            tmp = arr[i]
-            arr[i] = cur_max
-            cur_max = max(cur_max, tmp)
-        return arr[:-1]
+#
+#
+#
+# if __name__ == '__main__':
+#     arr = [17, 18, 5, 4, 6, 1]
+#     print(Solution1299().replaceElements(arr))
 
 
+# 给定两个数组，编写一个函数来计算它们的交集。
+#
+# 示例 1:
+#
+# 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+# 输出: [2,2]
+# 示例 2:
+#
+# 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+# 输出: [4,9]
+# 说明：
+#
+# 输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
+# 我们可以不考虑输出结果的顺序。
+# 进阶:
+#
+# 如果给定的数组已经排好序呢？你将如何优化你的算法？
+# 如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+# 如果 nums2 的元素存储在磁盘上，磁盘内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/intersection-of-two-arrays-ii
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+class Solution350:
+    def intersect(self, nums1, nums2):
+        map1 = {}
+        result = []
 
+        for i in nums1:
+            if i in map1:
+                map1[i] += 1
+            else:
+                map1[i] = 1
 
-if __name__ == '__main__':
-    arr = [17, 18, 5, 4, 6, 1]
-    print(Solution1299().replaceElements(arr))
+        for j in nums2:
+            if j in map1:
+                result.append(j)
+                if map1[j] == 1:
+                    map1.pop(j)
+                else:
+                    map1[j] -= 1
+        print(result)
+        return result
+
+# s = Solution350()
+# s.intersect([4,9,5], [9,4,9,8,4])
